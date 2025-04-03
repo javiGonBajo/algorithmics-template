34f2df9d2f26ce8b354028f7ca562a2bf10fc355
unknown <UO300809@uniovi.es>
2025-04-03 02:37:25 +0200

    'update1'

--- algstudent/s7/NullPath.java
+++ algstudent/s7/NullPath.java
@@ -1,18 +1,33 @@
-package s6;
+package algstudent.s6;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class NullPath {
 	private int[][] weights;
-	private ArrayList<Integer> result;
-	private int counter;
-	private boolean found = false;
+	private LinkedList<Integer> result;
+	private boolean found;
 	
 	public NullPath(int n) {
+		newNullPath(n, true);
+	}
+	
+	public NullPath() {
+		
+	}
+	
+	public static void main(String[] args) {
+		NullPath np = new NullPath(Integer.parseInt(args[0]));
+	}
+	
+	private void printPath() {
+		System.out.println(result);
+	}
+
+	public void newNullPath(int n, boolean print) {
 		weights = new int[n][n];
-		counter = n;
-		result = new ArrayList<Integer>();
+		result = new LinkedList<Integer>();
+		found = false;
 		Random p = new Random();
 		for (int i = 0; i < weights.length; i++) {
 			for (int j = 0; j < weights.length; j++) {
@@ -22,35 +37,76 @@ public class NullPath {
 			}
 		}
 		
-		CalculateNullPath();
+		CalculateNullPath(print);
 	}
 
-	private void CalculateNullPath() {
-		found = auxiliarMethod(0);
-		if(!found)
+	private void CalculateNullPath(boolean print) {
+		result.add(0);
+		found = auxiliarMethod(1);
+		if(!found && print) {
 			 System.out.println("There is no solution");
+		}
+		else {
+			result.add(weights.length-1);
+			printPath();
+		}
+		
+		printMatrix();
+		
 	}
 	
 	private boolean auxiliarMethod(int current) {
-		result.add(current);
 		if(checkResult())
-			found = true;
-		else
-			if(!found)
-				auxiliarMethod(current + 1);
+			return true;
+		
+		int counter = 0;
+		int node;
+		int rep = 0;
+		while(!found && counter < weights.length - 2 && result.size() < weights.length) {
+			do {
+				node = (current + counter + rep++)%(weights.length - 2);
+			} while(result.contains(node) && result.size()!=weights.length-1);
+			
+			
+			if(result.size() < weights.length-1) {
+				result.add(node);
+				found = auxiliarMethod((node + 1)%(weights.length - 1));
+				if(!found)
+					result.removeLast();
+					counter++;
+			}
+			
+			else {
+				if(!found ) {
+					result.removeLast();
+					return false;
+				}
+			}
+		}		
+		
 		return found;
 	}
-
+	
 	private boolean checkResult() {
-		if(result.size() < weights.length)
+		if(result.size() < weights.length - 1)
 			return false;
 		
 		int value = 0;
-		for(int i = 0; i<result.size() - 2; i++) {
+		for(int i = 1; i<result.size() - 3; i++) {
 			value += weights[result.get(i)][result.get(i+1)];
 		}
-		return value < 99 && value > -99;
+		value += weights[result.getFirst()][weights.length-1];
+		value += weights[result.getLast()][weights.length-1];
+		return value <= 99 && value >= -99;
 	}
 	
-	
+	private void printMatrix() {
+		int n = weights.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				System.out.print(String.format("%10s", weights[i][j]));
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
---
+++ algstudent/s7/NullPathTimes.java
@@ -0,0 +1,18 @@
+package algstudent.s6;
+
+public class NullPathTimes {
+	public static void main(String[] args) {
+		long t1, t2;
+		NullPath np = new NullPath();
+		for (int n = 20; n <= 1000000000; n += 5) {
+			t1 = System.currentTimeMillis();
+			
+			for (int i = 0; i <= 100; i++) {
+				np.newNullPath(n, false);
+			}
+
+			t2 = System.currentTimeMillis();
+			System.out.println(n + "\t" + (t2 - t1)/100);
+		}
+	}
+}
